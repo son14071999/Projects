106c8666e1d51a619ae8ffadca11d358ce4647c6
son14071999 <50294838+son14071999@users.noreply.github.com>
2022-11-09 17:33:12 +0700

    app-voca init

--- projects/app-voca/src/app/app.component.ts
+++ projects/app-voca/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const translate = require("../../node_modules/translate")
+    translate.from = "en"
+    translate.to = "vi"
+    for(let i = 0; i <= 10000; i++) {
+      setTimeout(() => {
+        translate("Hello").then((resp: any) => {
+          console.log(i)
+          console.log(resp)
+        })
+      }, 20);
+    }
+  }
   title = 'app-voca';
 }
