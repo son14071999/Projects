c52b6ca825ba7863da51a16111bd05d5d12667b7
son14071999 <50294838+son14071999@users.noreply.github.com>
2022-11-11 16:13:23 +0700

    add image

--- projects/AngularMngUI/src/app/app.component.ts
+++ projects/AngularMngUI/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'AngularMngUI';
+  logoLogin = "https://ra-dev.mobica.vn/images/loginLogo.png";
+  icon = "https://ra-dev.mobica.vn/images/headerLogo.png"
 }
--- projects/AngularMngUI/src/app/app.module.ts
+++ projects/AngularMngUI/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,4 +16,9 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+
+  }
+
+ }
